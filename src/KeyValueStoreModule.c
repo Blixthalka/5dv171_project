bd07b188e4531d179fb1fdac900c714b19c8f15d
emilvanneback <vanneback@hotmail.com>
2017-09-22 11:02:16 +0200

    kobject testing

--- src/KeyValueStoreModule.c
+++ src/KeyValueStoreModule.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("c13enn & c13evk");
@@ -9,7 +11,7 @@ MODULE_DESCRIPTION("A key-value store.");
 static int __init key_value_store_init(void)
 {
     printk(KERN_INFO "Init key-value store.\n");
-    example_kobject = kobject_create_and_add("kobject_example", kernel_kobj);
+    struct kobject *example_kobject = kobject_create_and_add("kobject_example", kernel_kobj);
     if(!example_kobject) {
         return -ENOMEM;
     }
