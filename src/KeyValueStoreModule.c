87281388dc8f30ea098754863834da075c15c722
emilvanneback <vanneback@hotmail.com>
2017-09-22 10:55:25 +0200

    testing sysFs

--- src/KeyValueStoreModule.c
+++ src/KeyValueStoreModule.c
@@ -9,6 +9,12 @@ MODULE_DESCRIPTION("A key-value store.");
 static int __init key_value_store_init(void)
 {
     printk(KERN_INFO "Init key-value store.\n");
+    example_kobject = kobject_create_and_add("kobject_example", kernel_kobj);
+    if(!example_kobject) {
+        return -ENOMEM;
+    }
+    kobject_put(example_kobject);
+
     return 0;
 }
 
