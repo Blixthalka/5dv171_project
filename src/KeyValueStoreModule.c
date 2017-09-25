8791e483b367b232aaa196171aa5cbf590726195
root <root@debian.se>
2017-09-25 13:39:02 +0200

    added const modifier

--- src/KeyValueStoreModule.c
+++ src/KeyValueStoreModule.c
@@ -12,18 +12,18 @@ MODULE_DESCRIPTION("A key-value store.");
 static struct kobject* kobj;
 static int test;
 
-static ssize_t test_show(struct kobject *obj, struct kobj_attribute *attr, char* buf)
+static ssize_t test_show(struct kobject *obj, struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%d\n",test);
 }
 
-static ssize_t test_store(struct kobject* obj, struct kobj_attribute *attr, char* buf, size_t count)
+static ssize_t test_store(struct kobject *obj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 	sscanf(buf, "%du", &test);
 	return count;
 }
 
-static struct kobj_attribute test_attribute = __ATTR(test,0660,test_show, test_store); 
+static struct kobj_attribute test_attribute = __ATTR(test, 0660, test_show, test_store); 
 
 static int __init key_value_store_init(void)
 {
