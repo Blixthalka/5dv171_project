8ff9ce06150c1af66a277b6309bca9855dd1ff4e
emilvanneback <vanneback@hotmail.com>
2017-09-26 11:04:51 +0200

    userspace and renamed src

---
+++ userSpace/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hashtable.h"
+
+
+int main(int argc, char *argv[]){
+    char* value = "Chicken Dinner";
+    unsigned int key = 1337;
+
+    insert(key, value);
+
+    printf("value = %d\n",get(key));
+
+
+}
+
+
