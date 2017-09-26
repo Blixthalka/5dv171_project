7689cc9e9f5687b9bd067339ebb3c5bdac58a10c
emilvanneback <vanneback@hotmail.com>
2017-09-26 11:23:40 +0200

    fixed tabs

--- userSpace/hashtable.c
+++ userSpace/hashtable.c
@@ -7,43 +7,44 @@
 #include <stdlib.h>
 
 
-char* create_entry(unsigned  int key, char* value);
-int store(char* entry);
+char *create_entry(unsigned int key, char *value);
 
-void insert(unsigned int key, char* value){
+int store(char *entry);
 
-    char* entry = create_entry(key, value);
-    store(entry);
+void insert(unsigned int key, char *value) {
+
+	char *entry = create_entry(key, value);
+	store(entry);
 }
 
-char* create_entry(unsigned int key, char* value){
-   return "test";
+char *create_entry(unsigned int key, char *value) {
+	return "test";
 }
 
-int store(char* entry){
-    FILE *fp;
+int store(char *entry) {
+	FILE *fp;
 
-    fp = fopen("/sys/kernel/key_value_store/test","wb");
-    if(!fp){
-        fprintf(stderr, "can not open file");
-        return -1;
-    }
+	fp = fopen("/sys/kernel/key_value_store/test", "wb");
+	if (!fp) {
+		fprintf(stderr, "can not open file");
+		return -1;
+	}
 
-    fwrite(entry, sizeof(entry), 1, fp);
+	fwrite(entry, sizeof(entry), 1, fp);
 
 }
 
-char* get(unsigned int key) {
-    FILE *fp;
-    char* value = (char*) malloc(1000);
-    if(!fp){
-        fprintf(stderr, "can not open file");
-    }
+char *get(unsigned int key) {
+	FILE *fp;
+	char *value = (char *) malloc(1000);
+	if (!fp) {
+		fprintf(stderr, "can not open file");
+	}
 
-    fp = fopen("/sys/kernel/key_value_store/test","rb");
+	fp = fopen("/sys/kernel/key_value_store/test", "rb");
 
-    fread(value,sizeof(value),1,fp);
+	fread(value, sizeof(value), 1, fp);
 
-    return value;
+	return value;
 
 }
--- userSpace/main.c
+++ userSpace/main.c
@@ -3,13 +3,13 @@
 #include "hashtable.h"
 
 
-int main(int argc, char *argv[]){
-    char* value = "Chicken Dinner";
-    unsigned int key = 1337;
+int main(int argc, char *argv[]) {
+	char *value = "Chicken Dinner";
+	unsigned int key = 1337;
 
-    insert(key, value);
+	insert(key, value);
 
-    printf("value = %d\n",get(key));
+	printf("value = %d\n", get(key));
 
 
 }
