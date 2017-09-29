f1112f44277fe4b1c2ea489013ba99b56e617cda
Blixthalka <emil837@gmail.com>
2017-09-29 11:15:36 +0200

    user space interface taking place

--- userSpace/netlink_kvs.c
+++ userSpace/netlink_kvs.c
@@ -5,38 +5,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../common/kvs_protocol.h"
+#include "netlink_kvs.h"
 
-#define NETLINK_NITRO 17
-#define MAX_PAYLOAD 2048
 
 int main() {
-    struct sockaddr_nl s_nladdr, d_nladdr;
+    return 0;
+}
+
+
+void kvs_connection_init(struct kvs_connection *connection)
+{
+    connection->fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_KVS);
+
+    memset(&connection->src_addr, 0, sizeof(connection->src_addr));
+    connection->src_addr.nl_family = AF_NETLINK;
+    connection->src_addr.nl_pad    = 0;
+    connection->src_addr.nl_pid    = getpid();
+
+    bind(connection->fd, (struct sockaddr *) &connection->src_addr, sizeof(connection->src_addr));
+
+    memset(&d_nladdr, 0, sizeof(d_nladdr));
+    connection->dest_addr.nl_family = AF_NETLINK;
+    connection->dest_addr.nl_pad    = 0;
+    connection->dest_addr.nl_pid    = 0; /* to kernel */
+}
+
+void kvs_put(struct kvs_connection *connection, int key, char *value)
+{
+    struct kvs_msg msg = CREATE_KVS_MSG_PUT(key, value);
+    kvs_send_msg(connection, msg);
+}
+
+void kvs_get(struct kvs_connection *connection, int key, char *value)
+{
+    struct kvs_msg msg = CREATE_KVS_MSG_GET(key, value);
+    kvs_send_msg(connection, msg);
+}
+
+void kvs_del(struct kvs_connection *connection, int key)
+{
+    struct kvs_msg msg = CREATE_KVS_MSG_DEL(key, value);
+    kvs_send_msg(connection, msg);
+}
+
+void kvs_send_msg(struct kvs_connection *connection, struct kvs_msg *user_msg)
+{
     struct msghdr msg;
     struct nlmsghdr *nlh = NULL;
     struct iovec iov;
-    int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_NITRO);
 
-    char *msgk = "Chicken dinner.";
-    int key = 1337;
-    struct kvs_msg kvs_m = CREATE_KVS_MSG_PUT(key, msgk);
 
-    char buf[400];
-    serialize_kvs_msg(buf, &kvs_m);
-
-/* source address */
-    memset(&s_nladdr, 0, sizeof(s_nladdr));
-    s_nladdr.nl_family = AF_NETLINK;
-    s_nladdr.nl_pad = 0;
-    s_nladdr.nl_pid = getpid();
-    bind(fd, (struct sockaddr *) &s_nladdr, sizeof(s_nladdr));
-
-/* destination address */
-    memset(&d_nladdr, 0, sizeof(d_nladdr));
-    d_nladdr.nl_family = AF_NETLINK;
-    d_nladdr.nl_pad = 0;
-    d_nladdr.nl_pid = 0; /* destined to kernel */
-
-/* Fill the netlink message header */
+    /* Fill the netlink message header */
     nlh = (struct nlmsghdr *) malloc(100);
     memset(nlh, 0, 100);
     strcpy(NLMSG_DATA(nlh), " Mr. Kernel, Are you ready ?");
@@ -45,19 +64,13 @@ int main() {
     nlh->nlmsg_flags = 1;
     nlh->nlmsg_type = 0;
 
-/*iov structure */
-
     iov.iov_base = (void *) nlh;
     iov.iov_len = nlh->nlmsg_len;
 
-/* msg */
     memset(&msg, 0, sizeof(msg));
     msg.msg_name = (void *) &d_nladdr;
     msg.msg_namelen = sizeof(d_nladdr);
     msg.msg_iov = &iov;
     msg.msg_iovlen = 1;
-    sendmsg(fd, &msg, 0);
-
-    close(fd);
-    return (EXIT_SUCCESS);
+    sendmsg(connection->fd, &msg, 0);
 }
